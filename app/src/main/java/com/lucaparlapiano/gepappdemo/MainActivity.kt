471d6fefd8223ace75455a83b466255e3d0dca48
luca.parlapiano@gmail.com <luca.parlapiano@gmail.com>
2021-05-09 14:25:27 +0200

    Added bottombar and localization

--- app/src/main/java/com/lucaparlapiano/gepappdemo/MainActivity.kt
+++ app/src/main/java/com/lucaparlapiano/gepappdemo/MainActivity.kt
@@ -2,10 +2,31 @@ package com.lucaparlapiano.gepappdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Setting fragments label in topbar
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.poiHomeFragment,
+                R.id.poiListFragment,
+                R.id.showPoiFragments
+            ))
+
+        //Setting navigator in the bottombar
+        var navController = findNavController(R.id.fragmentContainerView)
+        bottomNavigationView.setupWithNavController(navController)
+        bottomNavigationView.itemIconTintList = null
+
+        //Appy Label  on topbar
+        setupActionBarWithNavController(navController,appBarConfiguration)
     }
 }
---
+++ app/src/main/java/com/lucaparlapiano/gepappdemo/fragments/PoiListFragment.kt
@@ -0,0 +1,24 @@
+package com.lucaparlapiano.gepappdemo.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lucaparlapiano.gepappdemo.R
+
+
+class PoiListFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_poi_list, container, false)
+    }
+
+}
